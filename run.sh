5aaaad6481d362c48ef3c4005634b9b765f9768a
tyler <tpmgbq@umsystem.edu>
2026-08-16 19:50:30 -0500

    Update

--- run.sh
+++ run.sh
@@ -2,4 +2,7 @@
 
 #!/bin/bash
 emcc Para_Score.cpp -o Para_score.js -s EXPORTED_FUNCTIONS='["_calculateScore"]' -s EXPORTED_RUNTIME_METHODS='["ccall"]'
-npx serve
+git add .
+git commit -a -m "Update"
+git push
+npx vercel --prod
